1a8c0968d385afa9a3de884fc15c6262e3a6b5d5
Jaremy Creechley <creechley@gmail.com>
2020-07-07 18:07:37 -0600

    fixing gpio compile issues

--- nimsrc/libwidgetlords-2.1.0/include/gpio.h
+++ nimsrc/libwidgetlords-2.1.0/include/gpio.h
@@ -11,9 +11,15 @@
 #define GPIO_OPEN_DRAIN   4 // not implemented yet
 #define GPIO_OPEN_SOURCE  8 // not implemented yet
 
-void gpio_init();
-void gpio_configure(uint32_t pin, uint8_t flags);
-void gpio_write(uint32_t pin, uint8_t value);
+#define ERR_GPIO_PIN_INVALID -1
+#define ERR_GPIO_FILE_DESCRIPTOR  -2 
+#define ERR_GPIO_CHIP_INFO -3
+#define ERR_GPIO_PIN_LINEHANDLE -4
+#define ERR_GPIO_PIN_NUMBER_INVALID -5
+
+int8_t gpio_init();
+int8_t gpio_configure(uint32_t pin, uint8_t flags);
+int8_t gpio_write(uint32_t pin, uint8_t value);
 int8_t gpio_read(uint32_t pin);
 
 #endif
