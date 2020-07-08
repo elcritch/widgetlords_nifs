cd1bba9d19d1bac522dec8a001faf2c4c0cb5a70
Jaremy Creechley <creechley@gmail.com>
2020-07-07 19:40:03 -0600

    adding pi_spi functions

--- lib/widgetlords_nifs.ex
+++ lib/widgetlords_nifs.ex
@@ -2,24 +2,60 @@ defmodule WidgetLordsNifs do
   require Logger
   @on_load :load_nif
 
-  @type uint32_t :: non_neg_integer()
-  @type uint8_t :: non_neg_integer()
-  @type int8_t :: integer()
-  @type int32_t :: integer()
-
+  @type uint32 :: non_neg_integer()
+  @type uint16 :: non_neg_integer()
+  @type uint8 :: non_neg_integer()
+  @type int8 :: integer()
+  @type int32 :: integer()
+#
   def add_numbers(_a, _b), do: raise "not implemented"
   @spec gpio_init() :: :ok | {:error, integer()}
   def gpio_init(), do: raise "not implemented"
 
-  @spec gpio_configure(pin :: uint32_t(), flags: uint32_t()) :: int8_t()
+  @spec gpio_configure(pin :: uint32(), flags: uint32()) :: int8()
   def gpio_configure(_pin, _flags), do: raise "not implemented"
 
-  @spec gpio_write(pin :: uint32_t(), value :: uint8_t()) :: int8_t()
+  @spec gpio_write(pin :: uint32(), value :: uint8()) :: int8()
   def gpio_write(_pin, _value), do: raise "not implemented"
 
-  @spec gpio_read(pin :: uint32_t()) :: int8_t()
+  @spec gpio_read(pin :: uint32()) :: int8()
   def gpio_read(_pin), do: raise "not implemented"
 
+
+  # @spec spi_open(channel :: uint8, speed :: uint32) :: :ok
+  # def spi_open(_channel, _speed), do: raise "not implemented"
+  # @spec spi_transfer(data :: list(uint8), len :: uint32) :: :ok
+  # def spi_transfer(_data, _len), do: raise "not implemented"
+
+  @spec pi_spi_init() :: :ok
+  def pi_spi_init(), do: raise "not implemented"
+
+  @spec pi_spi_2ao_write_single(channel :: uint8, counts :: uint16, ctype :: uint8) :: :ok
+  def pi_spi_2ao_write_single(_channel, _counts, _ctype), do: raise "not implemented"
+
+  @spec pi_spi_8ai_read_single(channel :: uint8, ctype :: uint8) :: uint16
+  def pi_spi_8ai_read_single(_channel, _ctype), do: raise "not implemented"
+
+  @spec pi_spi_8di_read(address :: uint8, ctype :: uint8) :: uint8
+  def pi_spi_8di_read(_address, _ctype), do: raise "not implemented"
+
+  @spec pi_spi_8di_read_single(address :: uint8, channel :: uint8, ctype :: uint8) :: uint8
+  def pi_spi_8di_read_single(_address, _channel, _ctype), do: raise "not implemented"
+
+  @spec pi_spi_8di_init(address :: uint8, ctype :: uint8) :: :ok
+  def pi_spi_8di_init(_address, _ctype), do: raise "not implemented"
+
+  @spec pi_spi_8ko_write(data :: uint8, ctype :: uint8) :: :ok
+  def pi_spi_8ko_write(_data, _ctype), do: raise "not implemented"
+
+  @spec pi_spi_8ko_write_single(channel :: uint8, data :: uint8, ctype :: uint8) :: :ok
+  def pi_spi_8ko_write_single(_channel, _data, _ctype), do: raise "not implemented"
+
+  @spec pi_spi_8ai_16b_set_channel(channel :: uint8, ctype :: uint8) :: :ok
+  def pi_spi_8ai_16b_set_channel(_channel, _ctype), do: raise "not implemented"
+
+  @spec pi_spi_8ai_16b_read(ctype :: uint8) :: uint16
+  def pi_spi_8ai_16b_read(_ctype), do: raise "not implemented"
 
   def load_nif do
     sofile =
